74dd056a24ca4bd21dd5e3924939723acb05b453
Ho Yik Him <hoyikhim@Hims-MacBook-Pro.local>
2016-02-24 17:55:04 +0800

    parse input to GPB

--- ServerThread.java
+++ ServerThread.java
@@ -26,37 +26,14 @@ public class ServerThread extends Thread {
 		System.out.println("Accepted connection. ");
 
 		try {
-			byte[] resultBuff = new byte[0];
-		    byte[] buff = new byte[1024];
-		    int k = -1;
+			InputStream input = client.getInputStream();
 
-
-		    
-		    while((k = client.getInputStream().read(buff, 0, buff.length)) > -1) {
-		        byte[] tbuff = new byte[resultBuff.length + k]; // temp buffer size = bytes already read + bytes last read
-		        System.arraycopy(resultBuff, 0, tbuff, 0, resultBuff.length); // copy previous bytes
-		        System.arraycopy(buff, 0, tbuff, resultBuff.length, k);  // copy current lot
-		        resultBuff = tbuff; // call the temp buffer as your result buff
-		    }
-		    System.out.println(resultBuff.length + " bytes read.");
-		    System.out.println(resultBuff);
-		    
-		    InputStream is = client.getInputStream();
-		    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-			int nRead;
-			byte[] data = new byte[16384];
-
-			while ((nRead = is.read(data, 0, data.length)) != -1) {
-			  buffer.write(data, 0, nRead);
-			}
-
-			buffer.flush();
-
-			System.out.println(buffer.toByteArray());
-
-		    // EnvironmentMonitoringProtos.EnvironmentUpdate.SensorRecord sensorData = EnvironmentMonitoringProtos.EnvironmentUpdate.SensorRecord.parseFrom(resultBuff.toByteArray());
-
+			EnvironmentMonitoringProtos.EnvironmentUpdate sensorData = EnvironmentMonitoringProtos.EnvironmentUpdate.parseFrom(input);
+			for (EnvironmentMonitoringProtos.EnvironmentUpdate.SensorRecord data: sensorData.getSensorRecordList()) {
+		      // print some record;
+		      System.out.println("Timestamp: " + data.getTimestamp());
+		      System.out.println("Temp: " + data.getTemperature());
+		      }
 		}
 		catch (IOException e) {
 			e.printStackTrace();
